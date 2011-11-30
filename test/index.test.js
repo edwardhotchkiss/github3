1dc46d720ec7971e252edd062f7417ee240fcd3a
Edward Hotchkiss <e@ingk.com>
2011-11-30 15:22:40 -0800

    basic api call with tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,16 @@
 
-var vows = require("vows");
-var assert = require("assert");
+var vows = require('vows');
+var assert = require('assert');
 
-var github3 = require("../lib/github3");
+var github3 = new require('../lib/github3');
 
-vows.describe("General Module Tests").addBatch({
-  "when instantiating github3":{
+vows.describe('general module tests').addBatch({
+  'when instantiating github3':{
     topic:function(){ 
       return github3;
     },
-    "github3 should be a function":function(topic) {
-      assert.equal(typeof(topic), "function");
+    'github3 should be a function':function(topic) {
+      assert.equal(typeof(topic), 'function');
     }
   }
 }).export(module);
