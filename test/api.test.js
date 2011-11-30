745c86e4b696d05255d076e1bfa2c31c1602abd0
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-11-30 15:37:33 -0800

    tests for all public (and private) methods

--- test/api.test.js
+++ test/api.test.js
@@ -5,9 +5,50 @@ var assert = require('assert');
 var github3 = new require('../lib/github3');
 
 vows.describe('api tests').addBatch({
-  'when making a call to /orgs/ingklabs/public_members':{
+  // _get
+  'when making a generic _get call to /orgs/ingklabs/public_members':{
     topic:function(){ 
       github3._get('/orgs/ingklabs/public_members', this.callback);
+    },
+    'we should receive no errors, and data back':function(error, data) {
+      assert.equal(error, null);
+      assert.equal(typeof(data), 'object');
+    }
+  },
+  // getUser
+  'when making a call to getUser(edwardhotchkiss,':{
+    topic:function(){ 
+      github3.getUser('edwardhotchkiss', this.callback);
+    },
+    'we should receive no errors, and data back':function(error, data) {
+      assert.equal(error, null);
+      assert.equal(typeof(data), 'object');
+    }
+  },
+  // getOrgMembers
+  'when making a call to getOrgMembers(ingklabs,':{
+    topic:function(){ 
+      github3.getOrgMembers('ingklabs', this.callback);
+    },
+    'we should receive no errors, and data back':function(error, data) {
+      assert.equal(error, null);
+      assert.equal(typeof(data), 'object');
+    }
+  },
+  // getUserRepos
+  'when making a call to getUserRepos(edwardhotchkiss,':{
+    topic:function(){ 
+      github3.getUserRepos('edwardhotchkiss', this.callback);
+    },
+    'we should receive no errors, and data back':function(error, data) {
+      assert.equal(error, null);
+      assert.equal(typeof(data), 'object');
+    }
+  },
+  // getUsersWatched
+  'when making a call to getUsersWatched(edwardhotchkiss,':{
+    topic:function(){ 
+      github3.getUsersWatched('edwardhotchkiss', this.callback);
     },
     'we should receive no errors, and data back':function(error, data) {
       assert.equal(error, null);
