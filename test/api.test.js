ee19ae0423396adb53e802de708454daf21f7db1
Mark Lussier <mlussier@gmail.com>
2012-01-29 08:19:31 -0800

    added method to retrieve a repositories languages

--- test/api.test.js
+++ test/api.test.js
@@ -84,6 +84,16 @@ vows.describe('api tests').addBatch({
       assert.equal(error, null);
       assert.equal(typeof(data), 'object');
     }
+  },
+  // getLanguages
+  'when making a call to getLanguages(github3,edwardhotchkiss,':{
+    topic:function(){ 
+      github3.getLanguages('github3','edwardhotchkiss', this.callback);
+    },
+    'we should receive no errors, and data back':function(error, data) {
+      assert.equal(error, null);
+      assert.equal(typeof(data), 'object');
+    }
   }
   
 }).export(module);
