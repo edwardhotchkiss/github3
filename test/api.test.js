1a91688fc5bd6e4cee60ee877ab541d3239b909d
Mark Lussier <mlussier@gmail.com>
2012-01-29 08:29:57 -0800

    added method/test to retrieve  repository forks

--- test/api.test.js
+++ test/api.test.js
@@ -125,6 +125,16 @@ vows.describe('api tests').addBatch({
       assert.equal(typeof(data), 'object');
     }
   },
+  // getForks
+  'when making a call to getForks(github3,edwardhotchkiss,':{
+    topic:function(){ 
+      github3.getForks('github3','edwardhotchkiss', this.callback);
+    },
+    'we should receive no errors, and data back':function(error, data) {
+      assert.equal(error, null);	  
+      assert.equal(typeof(data), 'object');
+    }
+  },
   
   
 }).export(module);
